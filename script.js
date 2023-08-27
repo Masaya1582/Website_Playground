86b98d29a0351666face5b88bbddf8f6a598e8d6
Masaya1582 <masa8y88@gmail.com>
2023-08-27 16:34:56 +0900

    ModalWindowView

---
+++ script.js
@@ -0,0 +1,17 @@
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("myModal");
+
+openModalBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
